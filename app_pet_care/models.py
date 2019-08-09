6d2d88b2ac7d67ba5fd36f0a05064509a6c9b80e
Ever <everchicasp@gmail.com>
2019-08-09 02:26:00 -0600

    Agregue interfaz para hacer cita de mascota

--- app_pet_care/models.py
+++ app_pet_care/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class pet_appointment(models.Model):
+    pet_name = models.CharField(max_length=200)
+    date = models.DateField()
+    hour = models.DateTimeField()
+"""
+class species(model.Model):
+    name_species = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=200)
+    
+class pet(model.Model):
+    name = models.CharField(max_length=200)
+    birthdate = models.DateField()
+    species_pet = models.IntegerField()
+"""
--- app_pet_care/views.py
+++ app_pet_care/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import pet_appointment
+from .forms import post_form_medical_appointment
 
 # Create your views here.
 def index(request):
-    return render(request,'pet_care/index.html',{})
+    return render(request,'pet_care/standard_pages/index.html',{})
 
+def medical_appointment(request):
+    form = post_form_medical_appointment()
+    return render(request, 'pet_care/user/medical_appointment.html',{'form':form})
