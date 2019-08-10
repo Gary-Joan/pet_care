f48e9a9a110f7adabc2a55894a339e54e6d950ec
gary <gortiz1490@gmail.com>
2019-08-10 00:11:37 -0600

    arreglando urls y validando form citas

--- app_pet_care/views.py
+++ app_pet_care/views.py
@@ -8,5 +8,13 @@ def index(request):
     return render(request,'pet_care/standard_pages/index.html',{})
 
 def medical_appointment(request):
-    form = post_form_medical_appointment()
+    if request.method == "POST":
+        form = post_form_medical_appointment(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.nombreMascota=request.form_name
+        
+            post.save()
+    else:
+        form = post_form_medical_appointment()
     return render(request, 'pet_care/user/medical_appointment.html', {'form':form})
