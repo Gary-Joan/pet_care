14e30ede98a6c92d8c33224a5679583d1b3af1da
gary <gortiz1490@gmail.com>
2019-08-13 16:00:53 -0600

    delete agenda app

--- app_pet_care/urls.py
+++ app_pet_care/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+app_name = 'cal'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^medical_appointment/$', views.medical_appointment, name='medical_appointment'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
+    url(r'^event/new/$', views.event, name='event_new'),
+	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 
 
 ]
