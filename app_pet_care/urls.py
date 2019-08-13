b6e21c0436a5b0f2553b222afbbd52369cd92b88
gary <gortiz1490@gmail.com>
2019-08-13 15:54:56 -0600

    arreglo de urls

--- app_pet_care/urls.py
+++ app_pet_care/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 app_name = 'cal'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'medical_appointment', views.medical_appointment, name='medical_appointment'),
+    url(r'^medical_appointment/$', views.medical_appointment, name='medical_appointment'),
     url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
     url(r'^event/new/$', views.event, name='event_new'),
 	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
