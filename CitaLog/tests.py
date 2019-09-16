edb3eb719cc6d6b5c87e2c3df8e5674d4aff2e25
Daniel Perez <dan192596@gmail.com>
2019-09-16 15:10:13 -0600

    Test Url Lista Mascotas
    Creacion de la prueba unitaria para la lista de mascotas para el CRUD de mascotas, se utiliza el model de citalog

--- CitaLog/tests.py
+++ CitaLog/tests.py
@@ -2,5 +2,9 @@ from django.test import TestCase
 from django.utils import timezone
 
 from .models import EventCita
-
 # Create your tests here.
+
+#Prueba para lista de mascotas
+def test_ListaMascotas_is_resolved(self):
+        url = reverse('log:ListaMascotas')
+        self.assertEquals(resolve(url).url_name,'ListaMascotas')
