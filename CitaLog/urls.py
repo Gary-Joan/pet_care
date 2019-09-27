3fc28d5c5769bfe2c0c34bfe9215386cdd2ad2b1
Daniel Perez <dan192596@gmail.com>
2019-09-26 22:15:02 -0600

    Primera version de pdf
    201404268

--- CitaLog/urls.py
+++ CitaLog/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'cita'
 urlpatterns = [
     url(r'^$', views.index, name='indexC'),
-
     url(r'^calendarC/$', views.CalendarView.as_view(), name='calendarC'),
     url(r'^eventC/newC/$', views.event, name='event_newC'),
     url(r'^eventC/editC/(?P<event_id>\d+)/$', views.event, name='event_editC'),
--- CitaLog/views.py
+++ CitaLog/views.py
@@ -12,7 +12,10 @@ import calendar
 from .models import EventCita
 from .utils import Calendar
 from .forms import EventForm
-
+##Agregado para poder realizar reporte
+from reportlab.pdfgen import canvas
+from django.http import FileResponse
+import io
 #MODIFICO CHICAS
 from app_pet_care.models import Veterinarian
 #FIN MODIFICACION
@@ -101,8 +104,36 @@ def BorrarMascota(request, NombreCliente, NombreMascota, Fecha, Hora):
     EventCita.objects.filter(pet_owner=NombreCliente, title=NombreMascota, start_time=Fecha, date_start_time=Hora).delete()
     ListaMascotas = EventCita.objects.values('title', 'pet_owner', 'race').distinct()
     return render(request, 'CRUD_Mascota/Read_Mascotas.html', {'ListaMascotas':ListaMascotas})
-
-def GenerarPDFInfoCita(request, NombreCliente, NombreMascota, Fecha, Hora):    
+   
+def GenerarPDFInfoCita(request, NombreCliente, NombreMascota, Fecha, Hora):
     ListaCitas = EventCita.objects.filter(pet_owner=NombreCliente, title=NombreMascota, start_time=Fecha, date_start_time=Hora)
-    return render(request, 'cita/GenerarPDFInfoCita.html', {'ListaMascotas':ListaMascotas})
-    
+    buffer = io.BytesIO()
+    p = canvas.Canvas(buffer)
+    i = 0
+    for MiCita in ListaCitas:
+        if i != 0:
+            break
+        i = i+1
+        p.drawString(50, 750, "Doctor que atendio: " + MiCita.doctor_name)
+        p.drawString(50, 730, "Dueño de mascota: " + MiCita.pet_owner)
+        p.drawString(50, 710, "Mascota: " + MiCita.pet_owner)
+        p.drawString(50, 690, "Raza: " + MiCita.race)
+        p.drawString(50, 670, "Descripcion: ")
+        i = 650
+        j = 100
+        linea = ""
+        for letra in MiCita.description:
+            if j > 500:
+                j = 100
+                i = i - 10
+                p.drawString(j, i,linea)
+                linea = ""
+            linea = linea + letra
+            j = j + 5
+        j = 100
+        i = i - 10
+        p.drawString(j, i,linea)
+    p.showPage()
+    p.save()
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
