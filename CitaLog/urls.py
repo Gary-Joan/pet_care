4f2b23d5100d32161988c9881236b14ab5376def
gary <gortiz1490@gmail.com>
2019-10-16 22:30:10 -0600

    add form and view

--- CitaLog/urls.py
+++ CitaLog/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     url(r'^ListaMascotas/$', views.ListaMascotas, name='ListaMascotas'),
     url(r'^ConfirmacionBorrarMascota/(?P<NombreCliente>.+)/(?P<NombreMascota>.+)$', views.ConfirmacionBorrarMascota, name='ConfirmacionBorrarMascota'),
     url(r'^BorrarDatoMascota/(?P<NombreCliente>.+)/(?P<NombreMascota>.+)/(?P<Fecha>.+)/(?P<Hora>.+)$', views.BorrarMascota, name='BorrarMascota'),
-    url(r'^GenerarPDFInfoCita/(?P<NombreCliente>.+)/(?P<NombreMascota>.+)/(?P<Fecha>.+)/(?P<Hora>.+)$', views.GenerarPDFInfoCita, name='GenerarPDFInfoCita')
+    url(r'^GenerarPDFInfoCita/(?P<NombreCliente>.+)/(?P<NombreMascota>.+)/(?P<Fecha>.+)/(?P<Hora>.+)$', views.GenerarPDFInfoCita, name='GenerarPDFInfoCita'),
+
+    #-----urls servicios
+    url(r'^crearServicio/$', views.New_Services, name='CrearServicio')
+
 ]
